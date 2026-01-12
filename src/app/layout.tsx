f7ce3e9f0f0640a4a4f520564939496889335dd2
anhb_cssoft <anhb@cssoft.jp>
2026-01-12 10:57:56 +0700

    17: update sitemap

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,6 +38,17 @@ export const metadata: Metadata = {
   formatDetection: {
     telephone: false,
   },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
 };
 
 export const viewport: Viewport = {
