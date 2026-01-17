3b4376c35aab192df8505a0138dd522adbc1c99d
anhb_cssoft <anhb@cssoft.jp>
2026-01-17 18:40:41 +0700

    68: Update robots.txt

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,11 +2,17 @@ import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-      disallow: ["/api/"],
-    },
+    rules: [
+      {
+        userAgent: "Mediapartners-Google",
+        allow: "/",
+      },
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: ["/api/", "/_next/", "/static/"],
+      },
+    ],
     sitemap: "https://easysplit.click/sitemap.xml",
   };
 }
