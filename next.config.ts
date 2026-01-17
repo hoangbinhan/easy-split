794fbb054ac47c1ccaa6e2a10259595c3de84347
anhb_cssoft <anhb@cssoft.jp>
2026-01-17 19:48:45 +0700

    74: Update config CSP

--- next.config.ts
+++ next.config.ts
@@ -23,12 +23,12 @@ const nextConfig: NextConfig = {
             key: "Content-Security-Policy",
             value: [
               "default-src 'self';",
-              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://pagead2.googlesyndication.com https://partner.googleadservices.com https://tpc.googlesyndication.com https://www.google-analytics.com https://static.cloudflareinsights.com;",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://pagead2.googlesyndication.com https://partner.googleadservices.com https://tpc.googlesyndication.com https://www.google-analytics.com https://static.cloudflareinsights.com https://*.adtrafficquality.google;",
               "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;",
               "font-src 'self' https://fonts.gstatic.com;",
-              "img-src 'self' blob: data: https://*.google-analytics.com https://*.googlesyndication.com https://pagead2.googlesyndication.com https://flagcdn.com;",
+              "img-src 'self' blob: data: https://*.google-analytics.com https://*.googlesyndication.com https://pagead2.googlesyndication.com https://flagcdn.com https://*.adtrafficquality.google;",
               "connect-src 'self' https://www.google-analytics.com https://analytics.google.com https://pagead2.googlesyndication.com https://api.web3forms.com https://*.adtrafficquality.google;",
-              "frame-src 'self' https://googleads.g.doubleclick.net https://tpc.googlesyndication.com https://www.google.com;",
+              "frame-src 'self' https://googleads.g.doubleclick.net https://tpc.googlesyndication.com https://www.google.com https://*.adtrafficquality.google;",
             ]
               .join(" ")
               .replace(/\s{2,}/g, " ")
